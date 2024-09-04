d77e42344daf215ec85cc4d8c87585a48f6cc3db
ElRacoon <lea.granier@outlook.fr>
2024-09-04 16:42:29 +0200

    Add image prop to BackgroundComponent for customizable background images

---
+++ src/components/BackgroundComponent.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  image: {
+    type: String,
+    default: null
+  },
+  darken: {
+    type: Boolean,
+    default: true
+  }
+});
+</script>
+
+<template>
+  <div :style="{ backgroundImage: image ? `url(${image})` : 'none', backgroundSize: 'cover', backgroundPosition: 'center' }" class="background">
+    <div v-if="darken" class="overlay"></div>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.background {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
---
+++ src/views/NordicView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import BackgroundComponent from "@/components/BackgroundComponent.vue";
+</script>
+
+<template>
+  <BackgroundComponent image="https://www.france-mineraux.fr/wp-content/uploads/2024/07/fond-mythologie-nordique.jpg">
+
+  </BackgroundComponent>
+</template>
+
+<style scoped>
+
+</style>
